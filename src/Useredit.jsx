7529d7646c13cbd36e6f6ed2036cd4217ad09d76
Akash4590 <akashjaved4590@gmail.com>
2026-03-13 10:11:28 +0500

    put method to update data

--- src/Useredit.jsx
+++ src/Useredit.jsx
@@ -1,5 +1,5 @@
 import { useEffect,useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Useredit(){
 const[firstName,setfirstName] = useState("");  
@@ -7,6 +7,7 @@ const[lastName,setlastName] = useState("");
 const[email,setemail] = useState("");              
 const[age,setage] = useState("");               
 const {id}  = useParams();
+const navigate = useNavigate();
 
 
 useEffect(()=>{
@@ -23,15 +24,43 @@ setlastName(data.lastName);
 setemail(data.email);
 setage(data.age);
 }
+const updateuserdata = async () => {
+  console.log(firstName, lastName, email, age);
+ const url = "http://localhost:3000/users/"+id;
+  let response = await fetch(url, {
+    method: "PUT",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      age
+    })
+  });
+  const data = await response.json();
+  console.log(data);
+  if(data){
+   alert("user data update");
+   navigate("/");           
+  }
+};
+
+
 return(
 <div className="text-center">
 <h1>edit user details</h1> 
-<input type="text" placeholder="enter name" value = {firstName} /> <br /> <br />           
-<input type="text" placeholder="enter lastname" value = {lastName} /> <br /> <br />           
+<input type="text" placeholder="enter name" value = {firstName}
+onChange={(event)=>setfirstName(event.target.value)} /> <br /> <br />           
+<input type="text" placeholder="enter lastname" value = {lastName}
+onChange={(event)=>setlastName(event.target.value)} /> <br /> <br />           
           
-<input type="text" placeholder="enter email" value = {email} /> <br /> <br />           
-<input type="text" placeholder="user age" value = {age} /> <br /> <br /> 
-<button className="bg-gray-500 px-4 py-2 text-white rounded-full">edit user</button>          
+<input type="text" placeholder="enter email" value = {email}
+onChange={(event)=>setemail(event.target.value)} /> <br /> <br />           
+<input type="text" placeholder="user age" value = {age} 
+onChange={(event)=>setage(event.target.value)}/> <br /> <br /> 
+<button className="bg-gray-500 px-4 py-2 text-white rounded-full"
+  onClick={updateuserdata}>
+  edit user
+</button>          
 </div>              
 )              
 }
