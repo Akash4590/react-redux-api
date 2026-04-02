5390991fba8ed0da8372c8a996d119918d071f59
Akash4590 <akashjaved4590@gmail.com>
2026-04-02 10:16:52 +0500

    i start redux

---
+++ src/redux/Redux.jsx
@@ -0,0 +1,9 @@
+function import { connect } from 'react-redux'
+function Redux(){
+              return(
+                   <div>
+                            <h1>react redux</h1>
+                   </div>         
+              )
+}
+export default Redux;
